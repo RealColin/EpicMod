9bd85137ae9782230d01318998026b73aecc03a6
Werner, Colin <crwgond@gmail.com>
2024-08-04 00:50:26 -0500

    biomes are loaded from the colors on the image now

--- src/main/java/github/realcolin/epicmod/worldgen/biome/BiomeImageLoader.java
+++ src/main/java/github/realcolin/epicmod/worldgen/biome/BiomeImageLoader.java
@@ -1,26 +1,51 @@
 package github.realcolin.epicmod.worldgen.biome;
 
+import github.realcolin.epicmod.EpicMod;
 import net.minecraft.resources.ResourceLocation;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
 
 public class BiomeImageLoader {
-    //private final BufferedImage image;
+    private final BufferedImage image;
+
+    private final String PATH = "assets/%s/map/map.png".formatted(EpicMod.MOD_ID);
 
     public BiomeImageLoader(ResourceLocation res) {
+        URL resource = getClass().getClassLoader().getResource(PATH);
+        try {
+            image = ImageIO.read(Objects.requireNonNull(resource));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+//        String path = res.getNamespace() + "/" + res.getPath();
+//        System.out.println(path);
+//
 //        File file = new File(path);
 //        try {
 //            this.image = ImageIO.read(file);
 //        } catch (IOException e) {
 //            throw new RuntimeException(e);
 //        }
+
+
     }
 
     public int getColorAtPixel(int x, int y) {
+        int width = image.getWidth();
+        int height = image.getHeight();
 
-        return -1;
+        if (x < 0 || x >= width || y < 0 || y >= height) {
+            return -1;
+        } else {
+            int pixelColor = image.getRGB(x, y) & 0x00FFFFFF;
+            System.out.println(pixelColor);
+            return pixelColor;
+        }
     }
 }
